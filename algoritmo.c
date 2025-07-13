c4c0d93dfc53748b1504e5c062ac1d90aeab0d08
José Marcos <marcos.prado@dcomp.ufs.br>
2025-07-13 13:36:01 -0300

    Leitura do da quantidade de entradas no arquivo
    loop que busca dentro do .pla por onde está ".i %d" e o armazena na variável "in"

--- algoritmo.c
+++ algoritmo.c
@@ -8,12 +8,26 @@ int QuiMc (FILE* tabelao){
         return 1;
     }
     printf("\nArquivo aberto e lido\n\n");
+
+    int in = 0;
+    while (1){
+        int scanValido = fscanf(tabelao, ".i %d", &in);
+        if (scanValido == 1) break;
+        if (feof(tabelao) || scanValido == EOF) break;
+        fgetc(tabelao);
+    }
+    if (!in){
+        printf("\nNumero de entradas nao encontrado ou invalido\n\n");
+        return 2;
+    }
+    printf("\nNumero de entradas: %d\n\n", in);
     
     fclose(tabelao);
     return 0;
 }
 /*  Mapa de excecoes QuiMc:
     Excecao 1: ponteiro NULL
+    Excecao 2: numero invalido de entradas no arquivo
 */
 
 /*Funcao checa se ha ".pla" no final do arquivo*/
