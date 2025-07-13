9c4b95f81b0d9a498acc5c50484eca711a15509c
José Marcos <marcos.prado@dcomp.ufs.br>
2025-07-13 12:53:13 -0300

    Tratamento de algumas exceções
    tratamento de exceções geradas ao tentar abrir e ler o arquivo e ao receber formatos inválidos de argumentos na main

--- algoritmo.c
+++ algoritmo.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
+/*Funcao que implementea o algoritmo de Quine-McCluskey*/
 int QuiMc (FILE* tabelao){
+    if (!tabelao){
+        printf("Nao foi possivel abrir este arquivo\n");
+        return 1;
+    }
+
     
     fclose(tabelao);
 }
+/*  Mapa de excecoes QuiMc:
+    Excecao 1: ponteiro NULL
+*/
+
+/*Funcao checa se ha ".pla" no final do arquivo*/
+int encontrouPla(const char* nome) {
+        char *doPonto = strrchr(nome, '.'); //conteudo da string a partir do ultimo ponto
+        if (doPonto) {
+            return !strcmp(doPonto, ".pla");
+        }
+        return 0;
+    }
 
 int main (int argc, char *argv[]) {
     if (argc > 1) {
+        if (!encontrouPla(argv[1])){
+            printf("Formato inválido\n");
+            return 2;
+        }
         QuiMc(fopen(argv[1], "r"));
     }
     else {
@@ -15,3 +38,7 @@ int main (int argc, char *argv[]) {
     }
     return 0;
 }
+/*  Mapa de excecoes main:
+    Excecao 1: quantidade de argumentos inválida
+    Excecao 2: formato inválido
+*/
