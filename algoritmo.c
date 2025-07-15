c221e0892f6b891710dac827ebb0c86a52d84188
José Marcos <marcos.prado@dcomp.ufs.br>
2025-07-15 15:19:07 -0300

    Código detecta os mintermos '1' no arquivo

--- algoritmo.c
+++ algoritmo.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+void imprimeArrayInt(int* array, int tamanho){
+    printf("[");
+    for (int i = 0; i < tamanho-1; i++) printf("%d, ", array[i]);
+    printf("%d]", array[tamanho-1]);
+}
 
 /*Funcao que busca dentro de um arquivo .pla pelo numero de entradas*/
 int buscaEntradas(FILE* tabela){
+    rewind(tabela);
     int entradas = 0;
     while (1){
         int scanValido = fscanf(tabela, ".i %d", &entradas);
@@ -13,14 +22,58 @@ int buscaEntradas(FILE* tabela){
     return entradas;
 }
 
+int achaMintermos1(FILE* tabela, int entradas){
+    rewind(tabela);
+
+    char linha[entradas+4];
+    if (!linha){
+        printf("\nErro ao tentar percorrer o arquivo\n\n");
+        return -1;
+    }
+    int quantidadeDeMinTermos1 = 0;
+
+    while(fgets(linha, sizeof(linha), tabela)){
+        if (linha[0] == '.') continue;
+        if (linha[strlen(linha) - 2] == '1') quantidadeDeMinTermos1++;
+    };
+
+    return quantidadeDeMinTermos1;
+}
+
+void armazenaMintermos1(FILE* tabela, int entradas, int* mintermos1){
+    rewind(tabela);
+
+    char linha[entradas+4];
+    int numeroDaLinha = 0;
+    int i = 0;
+
+    while(fgets(linha, sizeof(linha), tabela)){
+        if (linha[0] == '.') continue;
+        if (linha[strlen(linha)-2] == '1') {
+            mintermos1[i] = numeroDaLinha;
+            i++;
+        }
+        numeroDaLinha++;
+    }
+}
+
 /*Funcao que implementea o algoritmo de Quine-McCluskey*/
 int QuiMc (FILE* tabela){
-    int in = buscaEntradas(tabela);
-    if (!in) {
+    int entradas = buscaEntradas(tabela);
+    if (!entradas) {
         printf("\nNumero de entradas nao encontrado ou invalido\n\n");
         return 1;
     }
-    printf("\nNumero de entradas: %d\n\n", in); 
+    printf("\nNumero de entradas: %d\n\n", entradas);
+
+    int quantiaMintermos1 = achaMintermos1(tabela, entradas);
+    int mintermos1[quantiaMintermos1];
+    armazenaMintermos1(tabela, entradas, mintermos1);
+
+    printf("\nQuantidade de mintermos 1: %d\n", quantiaMintermos1);
+    printf("Mintermos 1: ");
+    imprimeArrayInt(mintermos1, quantiaMintermos1);
+    printf("\n\n");
     
     fclose(tabela);
     return 0;
